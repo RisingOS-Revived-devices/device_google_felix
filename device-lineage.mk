5787f1d4e6f9d1d1a922596c83248fd0fa0e419c
Austin Young <austineyoung2000@gmail.com>
2024-12-30 09:32:38 -0500

    felix: Nuke HBM
    Change-Id: Ia166fbf22866eb6d8523c5e5cbdc725efe943139

--- device-lineage.mk
+++ device-lineage.mk
@@ -21,9 +21,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_SYSTEM_SERVER_JARS += \
     system_ext:felix-services
 
-# HBM
-PRODUCT_PACKAGES += \
-    HbmSVManagerOverlayFelix
+# PixelParts
+-include packages/apps/PixelParts/device.mk
 
 # IWLAN
 PRODUCT_PACKAGES += \
@@ -31,6 +30,9 @@ PRODUCT_PACKAGES += \
 
 # Basic Call Recorder
 include vendor/bcr/bcr.mk
+
+# Kernel
+TARGET_PREBUILT_KERNEL := device/google/felix-kernel/Image.lz4
 
 # wireless_charger HAL service
 include device/google/gs-common/wireless_charger/wireless_charger.mk
