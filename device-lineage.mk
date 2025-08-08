8c57811e2da2db65f579ba9d2a15afc393a1f762
Austin Young <austineyoung2000@gmail.com>
2025-08-08 16:25:44 -0400

    felix: Enables Google Face Unlock and BCR support
    Enables Google Face Unlock feature for the device.
    Adds a flag to enable prebuilt Basic Call Recorder.

--- device-lineage.mk
+++ device-lineage.mk
@@ -28,8 +28,8 @@ PRODUCT_SYSTEM_SERVER_JARS += \
 PRODUCT_PACKAGES += \
     Iwlan
 
-# Basic Call Recorder
-include vendor/bcr/bcr.mk
+# Ship Google Face Unlock ( GFU )Add commentMore actions
+TARGET_SUPPORTS_GFU := true
 
 # PixelSupport
 include device/google/gs-common/pixelsupport/pixelsupport.mk
